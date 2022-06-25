9f720433487c4675de21d5ff929b391953c5ac03
João Geonizeli <joao.geonizeli@gmail.com>
2022-06-25 14:04:47 -0300

    add header template

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :lesson_categories
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "lesson_categories#index"
 end
